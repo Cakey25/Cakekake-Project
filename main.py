c6b6629fafe1a7b5df780141cbd8a68e54c7414c
Cakey25 <cakey25yt@gmail.com>
2021-11-20 22:47:35 +0000

    First test

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 
-print('Hello World')
+import pygame
 
-print('Hello Github')
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+clock = pygame.time.Clock()
 
-print('Test3')
+player_surf = pygame.image.load('player_walk_1.png')
+player_rect = player_surf.get_rect(topleft = (0,0))
 
-print('Test4')
+while True:
+    for event in pygame.event.get():#
+        if event.type == pygame.QUIT:
+            pygame.exit()
+            exit()
+
+    screen.blit(player_surf,player_rect)
+
+    pygame.display.update()  # Update the display
+    clock.tick(60)
